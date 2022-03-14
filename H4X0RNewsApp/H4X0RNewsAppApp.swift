8f9493b1e93c507b6bc639f1cde00285bf9bf7fb
Matheus Diniz <matheus.diniiiiz@gmail.com>
2022-03-14 20:20:28 +0000

    Initial Commit

---
+++ H4X0RNewsApp/H4X0RNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RNewsAppApp.swift
+//  H4X0RNewsApp
+//
+//  Created by Matheus Diniz on 14/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
